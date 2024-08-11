5b69513a42ca8b20f8b022686f4a38de6feb63e2
qian.chen5 <1518083412@qq.com>
2024-08-11 18:39:34 +0800

    init

---
+++ src/main/java/org/cloud/jenkins_demo/Controller.java
@@ -0,0 +1,12 @@
+package org.cloud.jenkins_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/org/cloud/jenkins_demo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cloud.jenkins_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
